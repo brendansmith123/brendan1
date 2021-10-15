84a8e0bc1dc6cfdd7d8ec5f28c643334e690142d
Brendan Smith <brendansmith@Brendans-MacBook-Air.local>
2021-10-14 23:44:09 -0500

    First Commit

---
+++ data.js
@@ -0,0 +1,75 @@
+const data = [{
+    "id": 1,
+    "name": "mocha 1",
+    "image": "https://cdn.shopify.com/s/files/1/0408/9909/products/AirJordan1RetroHighOGDarkMocha-555088-105-Feature-LV012.jpg?v=1603740947",
+    "desc": 'Air Jordan 1 "Mocha"',
+    "price": 550
+  }, {
+    "id": 2,
+    "name": 'Off White Nike Dunk Lot 31',
+    "image": 'https://cdn.thesolewomens.co.uk/2021/08/off-white-x-nike-dunk-low-white-orange-lot-31.jpg',
+    "desc": "Off White Nike Dunk Lot 31",
+    "price": 500
+  }, {
+    "id": 3,
+    "name": 'Off White Air Force 1 "Lemonade"',
+    "image": "https://sneakernews.com/wp-content/uploads/2021/07/Off-White-Nike-AIr-Force-1-Lemonade-DD1876-700-11.jpg",
+    "desc": 'Off White Air Force 1 "Lemonade"',
+    "price": 2100
+  }, {
+    "id": 4,
+    "name": 'Air Jordan 6 Travis Scott "British Khacki"',
+    "image": "https://images.complex.com/complex/images/c_crop,h_1052,w_1871,x_65,y_564/c_fill,dpr_auto,f_auto,q_90,w_1400/fl_lossy,pg_1/xzebj5p9ilvdtgpnnig2/travis-scott-air-jordan-6-british-khaki-dh0690-200-pair",
+    "desc": 'Air Jordan 6 Travis Scott "British Khacki"',
+    "price": 550
+  }, {
+    "id": 5,
+    "name": 'Air Jordan 1 "Fragment"',
+    "image": "https://cdn.weartesters.com/wp-content/uploads/2014/12/Air-Jordan-1-x-Fragment-–-Up-Close-Personal5.jpg",
+    "desc": "Air Jordan 1 X Fragment",
+    "price": 4999
+  }, {
+    "id": 6,
+    "name": 'Yeezy 700 "Wave Runner"',
+    "image": "https://sneakernews.com/wp-content/uploads/2021/04/adidas-Yeezy-Boost-700-Wave-Runner-2021-2.jpg",
+    "desc": 'Yeezy 700 "Wave Runner"',
+    "price": 475
+  }, {
+    "id": 7,
+    "name": 'Yeezy Foam Runner "Sand"',
+    "image": "https://sneakernews.com/wp-content/uploads/2021/05/yeezy-foam-runner-sand-FY4567-may-release-1.jpg",
+    "desc": 'Yeezy Foam Runner "Sand"',
+    "price": 360
+  }, {
+    "id": 8,
+    "name": 'Converse X Golf le Fleur "Flame"',
+    "image": "https://hypebeast.com/image/2019/09/golf-le-fleur-converse-chuck-70-flames-release-2.jpg",
+    "desc": 'Converse X Golf le Fleur "Flame"',
+    "price": 500
+  }, {
+    "id": 9,
+    "name": 'Air Jordan 1 X Travis Scott X Fragment',
+    "image": "https://sneakernews.com/wp-content/uploads/2021/07/fragment-travis-scott-air-jordan-1-retro-high-DH3227-105-6-1.jpg",
+    "desc": "Air Jordan 1 X Travis Scott X Fragment",
+    "price": 2600
+  }, {
+    "id": 10,
+    "name": 'Nike Dunk Low "University Green"',
+    "image": "https://sneakernews.com/wp-content/uploads/2021/04/Nike-Dunk-Low-Team-Green-DD1391-101-4.jpg?w=1140",
+    "desc": 'Nike Dunk Low "University Green"',
+    "price": 400
+  }, {
+    "id": 11,
+    "name": 'Air Jordan 4 "White Cement"',
+    "image": "https://images.solecollector.com/complex/image/upload/c_fill,fl_lossy,q_auto,w_328/air-jordan-4-retro-white-cement-2_ihl4m7",
+    "desc": 'Air Jordan 4 "White Cement"',
+    "price": 485
+  }, {
+    "id": 12,
+    "name": 'Air Jordan 4 X Union "Off Noir"',
+    "image": "https://images.solecollector.com/complex/images/c_crop,h_1125,w_2000,x_0,y_598/c_fill,dpr_2.0,f_auto,fl_lossy,q_auto,w_680/uig5o0lyrszqf9ckutho/union-air-jordan-4-retro-off-noir-dc9533-001-pair",
+    "desc": 'Air Jordan 4 X Union "Off Noir"',
+    "price": 800
+  }]
+  
+  export default data;
